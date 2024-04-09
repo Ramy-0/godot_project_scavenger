622e2e8d37bd62f53432a3af6f7a0b5a09c32151
Ramy-0 <ramyrahib@googlemail.com>
2024-04-09 21:48:14 +0200

    code cleaning

--- weapons/sub nodes/weapon_debug.gd
+++ weapons/sub nodes/weapon_debug.gd
@@ -3,11 +3,10 @@ extends Control
 @onready var ammo = $Ammo_DB
 @onready var reload = $Reload_DB
 
-func _ready():
-	pass # Replace with function body.
+@onready var parent = get_parent()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ammo.text = str(get_parent().mag)
-	reload.text = str(get_parent().reloadTimer.time_left)
+	rotation = - parent.rotation
+	ammo.text = str(parent.mag)
+	reload.text = str(parent.reloadTimer.time_left)
