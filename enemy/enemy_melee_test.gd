bb8221acf74505cb4cbda4edadda7392620e1263
Ramy-0 <ramyrahib@googlemail.com>
2024-04-10 21:14:21 +0200

    added an enemy

--- enemy/enemy_melee_test.gd
+++ enemy/enemy_melee_test.gd
@@ -3,3 +3,13 @@ extends CharacterBody2D
 @export var health: float = 100.0
 @export var dmg: float = 12.0
 
+
+
+func _on_activate_area_body_entered(body):
+	if body.is_in_group("Player"):
+		print("Player spoted")
+
+
+func _on_attack_area_body_entered(body):
+	if body.is_in_group("Player"):
+		print("bite")
