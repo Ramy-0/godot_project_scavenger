a0d31ef766fbd2b4f4e655a3fc543d820129c74f
Ramy-0 <ramyrahib@googlemail.com>
2024-06-18 03:46:30 +0300

    Added enemy parts branch

---
+++ enemy/classes/enemy_type_melee_1.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+class_name EnemyTypeMelee1
+
+@export var target: PlayerV1		#change to player class
+
+#stats
+@export var health : float
+@export var damage : float
+@export var speed : float
+
+#subnodes
+@export var stateChart : StateChart
+@export var navAgent : NavigationAgent2D
+@export var hitBox : CollisionShape2D
+@export var collisionBox : CollisionShape2D
+@export var animSprite : AnimatedSprite2D
+
+func _physics_process(delta):
+	if navAgent != null and navAgent.target != null:
+		var dir = to_local(navAgent.get_next_path_position()).normalized()
+		velocity = dir * speed
+		move_and_slide()
+
---
+++ enemy/enemy_melee_test_2.gd
@@ -0,0 +1,2 @@
+extends EnemyTypeMelee1
+
