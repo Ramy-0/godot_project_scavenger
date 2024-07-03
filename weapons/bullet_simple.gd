1ef3d1b59e50bddca983be7947aff39be940c544
Ramy-0 <ramyrahib@googlemail.com>
2024-07-03 22:03:47 +0300

    added a simple bullet (class)

---
+++ weapons/bullet_simple.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+class_name BulletClass
+
+var parent: CharacterBody2D
+var damage: int
+var speed: float
+
+var dir = Vector2.RIGHT
+
+func init(p_parent, p_damage, p_speed, p_lifetime, p_position, p_rotation):
+	parent = p_parent
+	damage = p_damage
+	speed = p_speed
+	position = p_position
+	rotation = p_rotation
+	$Timer.wait_time = p_lifetime
+	
+	dir = dir.rotated(rotation)
+
+func _physics_process(delta):
+	translate(dir.normalized() * speed * delta)
+
+
+func _on_timer_timeout():
+	queue_free()
