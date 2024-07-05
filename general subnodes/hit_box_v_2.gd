79b74f61b986ccd06b67e505b8293c8a3cdc690b
Ramy-0 <ramyrahib@googlemail.com>
2024-07-05 18:24:20 +0300

    small changes probably broke it

--- general subnodes/hit_box_v_2.gd
+++ general subnodes/hit_box_v_2.gd
@@ -5,24 +5,28 @@ class_name HitBoxV2
 @export var parent_group : String
 @export var target_group : String
 @export var damage : int
+@export var delete_after_hit : bool = false
 
 func _ready():
 	parent_group = get_parent().get_groups()[0]
 	add_to_group(parent_group)
-	#print(get_groups())
+	print(get_groups())
 	
 	if is_in_group("Player"):
 		target_group = "Enemy"
 	elif is_in_group("Enemy"):
 		target_group = "Player"
-	#print(target_group)
+	print(target_group)
 	
 	damage = get_parent().damage
 	
 	get_child(0).disabled = true
 
 func _on_area_entered(area):
-	#print("hit")
+	print("hitBox hit")
+	print(target_group, area.get_parent().get_groups())
 	if area.get_parent().is_in_group(target_group):
+		print("area attacked")
 		area.attacked(damage)
-		get_parent().queue_free()
+		if delete_after_hit:
+			get_parent().queue_free()
--- general subnodes/hurt_box_v_2.gd
+++ general subnodes/hurt_box_v_2.gd
@@ -5,6 +5,7 @@ class_name HurtBoxV2
 @export var parent : CharacterBody2D
 
 func _ready():
+	add_to_group(get_parent().get_groups()[0])
 	parent = get_parent()
 
 func attacked(damage):
