9d46fba0089c559ed292de7644afb6302324bd6f
Ramy-0 <ramyrahib@googlemail.com>
2024-07-05 17:55:02 +0300

    added a basic enemy still wip

---
+++ enemy/enemy sub nodes/enemy_nav_agent.gd
@@ -0,0 +1,24 @@
+extends NavigationAgent2D
+
+@export var target : Node2D
+@export var pathUpdateTime : float = 0.2
+
+@onready var updateTimer = $Update_Timer
+
+func _ready():
+	updateTimer.wait_time = pathUpdateTime
+	updateTimer.stop()
+
+func _physics_process(delta):
+	if target != null and updateTimer.is_stopped():
+		updateTimer.start()
+	
+	if get_parent().target != null and target == null:
+		target = get_parent().target
+
+func update_path():
+	if target != null:
+		target_position = target.global_position
+
+func _on_update_timer_timeout():
+	update_path()
---
+++ enemy/enemy_basic.gd
@@ -0,0 +1,54 @@
+extends EnemyClass
+
+@export var pre_atk_delay : float
+@export var post_atk_delay : float
+
+@onready var atkPreT = $Pre_Atk_Timer
+@onready var atkPostT = $Post_Atk_Timer
+
+func _ready():
+	atkPreT.wait_time = pre_atk_delay
+	atkPostT.wait_time = post_atk_delay
+	update_variables()
+
+func _physics_process(delta):
+	pass
+
+#AREA SIGNALS
+func _on_chasing_area_body_entered(body):
+	print("see u")
+	if body.is_in_group("Player") and target == null:
+		target = body
+		stateChart.send_event("player_in_sight")
+
+func _on_attacking_area_body_entered(body):
+	if body.is_in_group("Player") and body == target:
+		stateChart.send_event("player_in_attack_range")
+
+
+#STATE CHARTS SIGNALS
+func _on_chasing_a_state_state_entered():
+	#navRefT.start()
+	if $Attacking_Area.overlaps_body(target):
+		stateChart.send_event("player_in_attack_range")
+
+func _on_chasing_a_state_state_physics_processing(delta):
+	navigate_to(target)
+	look_at_path()
+
+func _on_attacking_a_state_state_entered():
+	#navRefT.stop()
+	look_at_target(target)
+	atkPreT.start()
+
+
+#TIMERS SIGNALS
+
+func _on_pre_atk_timer_timeout():
+	hitBoxColl.disabled = false
+	atkPostT.start()
+	await get_tree().create_timer(0.05).timeout
+	hitBoxColl.disabled = true
+
+func _on_post_atk_timer_timeout():
+	stateChart.send_event("attack_finished")
