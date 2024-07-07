76e0e2b86d18db033a9649f13cdccb89624ef5ec
Ramy-0 <ramyrahib@googlemail.com>
2024-07-07 22:09:03 +0300

    small fixes

--- players/subnodes/weapon_belt.gd
+++ players/subnodes/weapon_belt.gd
@@ -13,14 +13,15 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_just_pressed("kb_q"):
+		print("switch")
 		if weapon1.active and not weapon2.active:
 			weapon1.active = false
 			weapon2.active = true
-			weapon1.set_as_active()
-			weapon2.set_as_inactive()
+			weapon1.set_as_inactive()
+			weapon2.set_as_active()
 		elif weapon2.active and not weapon1.active:
 			weapon2.active = false
 			weapon1.active = true
-			weapon2.set_as_active()
-			weapon1.set_as_inactive()
+			weapon2.set_as_inactive()
+			weapon1.set_as_active()
 	
