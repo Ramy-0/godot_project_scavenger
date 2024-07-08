2310fa5f32cc12607255712d0a44a732e2c4817c
Ramy-0 <ramyrahib@googlemail.com>
2024-07-09 01:47:48 +0300

    added hitmarker sound and bullet trail

--- weapons/bullet_simple.gd
+++ weapons/bullet_simple.gd
@@ -9,9 +9,22 @@ var speed: float
 @export var hit_marker : PackedScene = preload("res://weapons/weapon sub nodes/hit_marker.tscn")
 
 var dir = Vector2.RIGHT
+var init_pos : Vector2
 
 func _ready():
 	contact_monitor = true
+	init_pos = global_position
+
+func _physics_process(delta):
+	translate(dir.normalized() * speed * delta)
+	queue_redraw()
+	#print(init_pos)
+
+func _draw():
+	pass
+	#print(global_position, position, init_pos)
+	#draw_line(Vector2.ZERO, Vector2(-abs(global_position.x-init_pos.x), 0), Color.RED, 2.0)
+	#draw_circle(Vector2.ZERO, 16, Color.AQUA)
 
 func init(p_parent, p_damage, p_speed, p_lifetime, p_position, p_rotation):
 	parent = p_parent
@@ -22,17 +35,19 @@ func init(p_parent, p_damage, p_speed, p_lifetime, p_position, p_rotation):
 	$Timer.wait_time = p_lifetime
 	
 	dir = dir.rotated(rotation)
+	
+	#init_pos = global_position
 
-func _physics_process(delta):
-	translate(dir.normalized() * speed * delta)
 
 func hit_mark(target):
 	var hm = hit_marker.instantiate()
 	hm.init(global_position)
 	MyFuncs.get_fst_parent_in(self, "World").add_child(hm)
 
+
 func _on_timer_timeout():
 	queue_free()
+
 
 func _on_area_2d_body_entered(_body):
 	queue_free()
