3b01de493d02e9077b4420b1d62b8909ef544bab
Ramy-0 <ramyrahib@googlemail.com>
2024-09-15 21:09:07 +0300

    THE CODE CULLING

--- ui/start_menu.gd
+++ ui/start_menu.gd
@@ -2,15 +2,15 @@ extends Control
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_packed(preload("res://worlds/world_1.tscn"))
-
+	pass
+	#get_tree().change_scene_to_packed(preload("res://worlds/world_1.tscn"))
 
 func _on_button_2_pressed():
 	get_tree().change_scene_to_packed(preload("res://worlds/world_2.tscn"))
 
-
 func _on_button_3_pressed():
-	get_tree().change_scene_to_packed(preload("res://worlds/world_test.tscn"))
+	pass
+	#get_tree().change_scene_to_packed(preload("res://worlds/world_test.tscn"))
 
 func _on_button_4_pressed():
 	get_tree().change_scene_to_packed(preload("res://worlds/world_director_test.tscn"))
